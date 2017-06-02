c353513bd3ce01d8e3d5cdd8c79cb53eeed8d02f
rvolkert <rachel.volkert@banno.com>
2017-06-02 10:16:35 -0500

    Starting Ch 7.

--- fp-in-scala/src/main/scala/Chapter7.scala
+++ fp-in-scala/src/main/scala/Chapter7.scala
@@ -2,18 +2,19 @@ package rvolkert.bookclub
 
 object Chapter7 {
 
-  type Par[A] // Par for parallel
+  // Par for parallel
+  class Par[A]
   object Par {
     //takes a value and evaluates it on another logical thread
     //begins evaluating as soon as it's referenced, since scala's left-to-right evaluation
     //would force it to be sequential if we waited until get was called on it
-    def unit[A](a: => A): Par[A] // does this need to be lazy, now that we have map2? 7.1
+    def unit[A](a: => A): Par[A] = ??? // does this need to be lazy, now that we have map2? 7.1
 
     //avoid using get besides at the edge of the program, since it needs to
-    def get[A](a: Par[A]): A
+    def get[A](a: Par[A]): A = ???
 
     // Ex 7.1: write the signature of map2
-    def map2[A,B](parA: => Par[A], parB: => Par[B])(f: (A, B) -> C): Par[C]
+    def map2[A,B,C](parA: => Par[A], parB: => Par[B])(f: (A, B) => C): Par[C] = ???
     //7.1 we conclude that because of l to r eval, we need these to be lazy so we aren't forced
     //to unpack the l side before getting to the r
   }
